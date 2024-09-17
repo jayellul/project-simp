af0e10fce72bda5d888d971c6d15ed76a725ca6a
jellul <jay.ellul@move.com>
2024-09-16 21:09:43 -0400

    password page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Password from './Password';
 
-function App() {
+const App = () => {
+  const [loggedIn, setLoggedIn] = React.useState(false);
+  const onSuccess = () => {
+    setLoggedIn(true);
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {!loggedIn && <Password onSuccess={onSuccess} />}
+        {loggedIn && (
+          <div>
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>Welcome to the secret area!</p>
+          </div>
+        )}
       </header>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Password.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import './App.css';
+
+const password = 'pikachu';
+
+export const Password = ({ onSuccess }) => {
+  const [input, setInput] = React.useState('');
+  const [attempts, setAttempts] = React.useState(0);
+
+  const remainingAttempts = 3 - attempts;
+  const showHint = attempts === 2;
+
+  const onChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const onSubmit = () => {
+    if (input.toLowerCase() === password) {
+      onSuccess();
+    } else {
+      setAttempts(attempts + 1);
+    }
+  };
+  return (
+    <div className="password">
+      {attempts >= 3 && <p>You can refresh and try again baby ;)</p>}
+      {attempts < 3 && (
+        <div>
+          <p>Enter the password:</p>
+          <div>
+            <input className="password-input" onChange={onChange} />
+          </div>
+          <button className="password-submit" onClick={onSubmit}>
+            Submit
+          </button>
+        </div>
+      )}
+      <p className="password-hint">Remaining attempts: {remainingAttempts}</p>
+      {showHint && (
+        <p className="password-hint">
+          Hint: Name of our first biological child
+        </p>
+      )}
+    </div>
+  );
+};
+
+export default Password;
