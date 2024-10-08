44a771b6d8ca8e9ddbb6fae17fac39e88b780aa7
jellul <jay.ellul@move.com>
2024-10-07 22:44:51 -0400

    moment

--- src/Yippie.js
+++ src/Yippie.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './Yippie.css';
 import flag from './img/flag.png';
+import moment from 'moment';
 
 export const Yippie = () => {
+  const timeSinceAsking = moment('20240918', 'YYYYMMDD').fromNow();
+
   return (
     <div className="yippie">
       <p className="yippie-text">YIPPIEEEE!! 👩🏼‍❤️‍💋‍👩🏻</p>
       <img className="yippie-img" src={flag} alt="flag" />
+      <p>I know I asked {timeSinceAsking} but still ;)</p>
     </div>
   );
 };
